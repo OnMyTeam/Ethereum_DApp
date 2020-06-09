cea148a50dfb1d20c4ab4081ae00bcf19317e445
MyTeamMyTeam <sangiki07@gmail.com>
2020-06-09 20:09:07 +0900

    add Token Wallet Page

---
+++ TokenWallet/contracts/Token.sol
@@ -0,0 +1,95 @@
+pragma solidity 0.6.4;
+import './Library.sol';
+import './Ownable.sol';
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract OSDCToken is IERC20, Ownable {
+    using SafeMath for uint;
+    
+    uint _totalSupply;
+    uint CABalance;
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) public allowence;
+
+    event Transfer(address indexed _from, address indexed _to, uint tokens);
+    event Approval(address indexed _tokenOwner, address indexed _spender, uint tokens);
+
+    constructor() public {
+        
+        symbol = "OSDC";
+        name = "osdc Token";
+        // totalSupply = 1000000 *10**uint256(decimals);
+        _totalSupply = 10000000000;
+        CABalance = _totalSupply;
+    }
+
+    function totalSupply() public override view returns (uint) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public override view returns (uint remaining) {
+        return allowence[tokenOwner][spender];
+    }
+    
+    function _transfer (address _from, address _to, uint256 _value) internal {
+        require(_to != address(0));
+        require(balances[_from] >= _value, "need token");
+        
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+    }
+
+    function transfer (address _to, uint256 _value) public override returns (bool success) {
+        _transfer(tx.origin, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from,address _to, uint256 _value) public override returns (bool success) {
+        require(_value <= allowence[_from][msg.sender]);
+        allowence[_from][msg.sender] = allowence[_from][msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve (address _spender, uint256 _value) public override returns (bool success) {
+        allowence[msg.sender][_spender] = _value;
+        emit Approval (msg.sender, _spender, _value);
+        return true;
+    }
+
+     function withdrawal(address buyer) external {
+        balances[buyer] = 0;
+    }
+
+    receive() external payable{
+        uint256 amount = msg.value;
+        require (CABalance >= amount);
+        CABalance = CABalance.sub(amount);
+        balances[msg.sender] = balances[msg.sender].add(amount);
+        
+        emit Transfer(owner, msg.sender, amount);
+    }
+
+    fallback() external {
+        revert();
+    }
+}
---
+++ TokenWallet/src/public/js/login.js
@@ -0,0 +1,83 @@
+LogIn = {
+  web3Provider: null,
+  contracts: {},
+
+  init: async function() {
+    await Init.init();
+    LogIn.bindEvents();
+    LogIn.getAccountList();
+  },
+
+  getAccountList: function() {
+    web3.eth.getAccounts(function (e, r) {
+      LogIn.refreshBalance(r);
+      LogIn.makeSelectAddress(r);
+    });
+  },
+
+  refreshBalance: async function(list) {
+    var token;
+    var html;
+    var ether;
+    for (var i = 0; i < list.length; i++) {
+      web3.eth.getBalance(list[i], (err, balance) => {
+        console.log(balance);
+        ether = parseInt(web3.utils.fromWei(balance, "ether"));
+        
+      });       
+      await Init.OSDCTokenInstance.balanceOf(list[i],{from:list[i]}).then(function(result) {
+        if(result.words[0] == undefined){
+          token = 0;
+        }else {
+          token = result.words[0];
+        }
+        html += "<tr>";
+        html += "<td>" + list[i] + "</td>";
+        html += "<td>" + ether.toFixed(2) + " ETH</td>";
+        html += "<td> " + token + " </td>";
+        html += "</tr>";
+      });
+    }
+    $('#tableContent').html(html);
+  },
+
+
+  makeSelectAddress: function(list) {
+    var html;
+    html += '<option value=""> Select </a>';
+    for (var i = 0; i < list.length; i++) {
+      html += '<option value="' + list[i] + '">' + list[i] + '</a>';
+    }
+    $('.custom-select').html(html);
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '.btn-primary', LogIn.logIn);
+  },
+
+
+  logIn: function() {
+    var loginForm = $('#loginform');
+    var address = $('#selectAccount').val();
+    if (address == ''){ alert('Please select address'); return; } 
+    
+    Init.membershipInstance.getMemberInfo({from:address}).then(function (result) {
+      // console.log(result);
+      if (result != 0x0) {
+        loginForm.attr('action','/shop');
+        loginForm.attr('method','post');
+        loginForm.submit();
+      }
+      else {
+        alert("Please Join First!");
+      }
+    });
+  }
+};
+
+$(function() {
+  $(window).load(function() {
+    LogIn.init();
+  });
+
+});
